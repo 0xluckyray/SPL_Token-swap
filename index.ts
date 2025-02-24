b7834ec948ac54385dfdf5924f0af2518c3cb96d
0xlukcyray <saivietthanh0314@gamil.com>
2025-02-24 09:11:05 +0200

    feat: Buy Sell Token feature - Completed

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Connection, Keypair, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL, VersionedTransaction } from "@solana/web3.js";
+import { Connection, Keypair, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL, VersionedTransaction, sendAndConfirmTransaction } from "@solana/web3.js";
 import { getOrCreateAssociatedTokenAccount, NATIVE_MINT } from "@solana/spl-token";
 import bs58 from "bs58";
 import dotenv from "dotenv";
@@ -12,20 +12,44 @@ const RPC_URL=process.env.RPC_URL as string;
 
 const anchorWallet = new Wallet(Keypair.fromSecretKey(bs58.decode(WALLET_PRIVATE_KEY)));
 
-async function SplTokenSwap(tokenAddress: string, amount: number) {
+async function SplTokenSwap(tokenAddress: string, amount: number, isBuySell: string) {
     const connection = new Connection(RPC_URL, "confirmed");
-    const tokenDecimals = getTokenDecimals(connection, tokenAddress);
+    const tokenDecimals = await getTokenDecimals(connection, tokenAddress);
     const slippageBps = parseInt(process.env.SLIPPAGEBPS || "") || 50;
-    
-    const swapInfo = await getSwapInfo(NATIVE_MINT.toBase58(), tokenAddress, amount * LAMPORTS_PER_SOL, slippageBps);
+    let swapInfo:any;
+    if(isBuySell == 'buy'){
+        swapInfo = await getSwapInfo(NATIVE_MINT.toBase58(), tokenAddress, amount * LAMPORTS_PER_SOL, slippageBps);
+    } else {
+        // sell
+        swapInfo = await getSwapInfo(tokenAddress, NATIVE_MINT.toBase58(), amount * Math.pow(10, tokenDecimals), slippageBps);
+    }
     const swapTransaction = await getSwapTransaction(swapInfo, anchorWallet);
     const swapTransactionBuf = Buffer.from(swapTransaction, "base64");
     const latestBlockHash = await connection.getLatestBlockhash();
-    const transaction = VersionedTransaction.deserialize(swapTransactionBuf);
-    transaction.message.recentBlockhash = latestBlockHash.blockhash;
-    transaction.sign([anchorWallet.payer]);
+    const versionedTransaction = VersionedTransaction.deserialize(swapTransactionBuf);
+    versionedTransaction.message.recentBlockhash = latestBlockHash.blockhash;
+    versionedTransaction.sign([anchorWallet.payer]);
 
-    console.log("Swap Quote Info:", transaction);
+    try{
+        const transactionSignature = await connection.sendTransaction(versionedTransaction, { maxRetries: 20 });
+        const confirmation = await connection.confirmTransaction({
+            signature: transactionSignature,
+            blockhash: latestBlockHash.blockhash,
+            lastValidBlockHeight: latestBlockHash.lastValidBlockHeight
+        }, "confirmed");
+
+        if(confirmation.value.err){
+            throw new Error("Transaction not confirmed");
+        }
+
+        console.log(
+            "Transaction Signature: ",
+            `https://solscan.io/tx/${transactionSignature}`
+          );
+    }
+    catch (error){
+        console.error("Error occurred during swap:", error);
+    }
 }
 
 async function getTokenDecimals(connection: Connection, tokenAddress: string) {
@@ -40,5 +64,8 @@ async function getTokenDecimals(connection: Connection, tokenAddress: string) {
 }
 
 const TOKEN_ADDRESS = "4aR3jtFKWuYzkNE27WG4V7Jt6DDhwKcc2qjzN5Tkpump"
-const amount = 0.001;
-SplTokenSwap(TOKEN_ADDRESS, amount);
+let amount:number;
+amount = 0.001;
+SplTokenSwap(TOKEN_ADDRESS, amount, 'buy');  // Buy token
+amount = 1000;
+SplTokenSwap(TOKEN_ADDRESS, amount, 'sell');  // Buy token
